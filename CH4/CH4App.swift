b5a4cca19f0cc0c743c1ed13319927d998d1dc90
Averina Harseno Subianto <156516558+lunarlife2@users.noreply.github.com>
2026-06-26 11:57:33 +0800

    initial

---
+++ CH4/CH4App.swift
@@ -0,0 +1,17 @@
+//
+//  CH4App.swift
+//  CH4
+//
+//  Created by Averina on 26/06/26.
+//
+
+import SwiftUI
+
+@main
+struct CH4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
