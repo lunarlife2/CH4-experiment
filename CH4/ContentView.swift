7537b1cdc8c514c1192c20f6ffae1bf690db0c5c
Ni Komang Ayu Juliana <124333724+AyuJuliana@users.noreply.github.com>
2026-06-30 14:10:00 +0800

    add new app

--- CH4/ContentView.swift
+++ CH4/ContentView.swift
@@ -1,24 +1,93 @@
 //
 //  ContentView.swift
-//  CH4
+//  RWhealthkit
 //
-//  Created by Averina on 26/06/26.
+//  Created by Ni Komang Ayu Juliana on 29/06/26.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var manager = HealthKitManager()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("HealthKit App")
+                .font(.largeTitle)
+            Text(manager.authStatus)
+                .font(.caption)
         }
         .padding()
+        
+        VStack{
+            HStack{
+                ZStack{
+                    Color(uiColor: .systemGray6)
+                        .cornerRadius(15)
+                    VStack {
+                        HStack (alignment: .top) {
+                            VStack(alignment: .leading, spacing: 5) {
+                                Text("Today Steps")
+                                    .font(.system(size:16))
+                                
+                            }
+                        }
+                        
+                        Text("\(manager.stepCount)")
+                            .font(.system(size: 50))
+                    }
+                    
+                }
+                ZStack{
+                    Color(uiColor: .systemGray6)
+                        .cornerRadius(15)
+                    VStack {
+                        HStack (alignment: .top) {
+                            VStack(alignment: .leading, spacing: 5) {
+                                Text("Today Calories")
+                                    .font(.system(size:16))
+                                
+                            }
+                        }
+
+                        Text("\(manager.activeEnergyBurned)")
+                            .font(.system(size: 50))
+                    }
+                    
+                }
+            }
+            VStack{
+                Button("Requested Permission"){
+                    Task{await manager.requestAuthorization()}
+                }
+                .buttonStyle(.borderedProminent)
+                
+                Button("Refresh Steps"){
+                    Task{await manager.fetchTodayStepCount()}
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.green)
+                
+                Button("Refresh Calories"){
+                    Task{await manager.fetchTodayEnergyBurned}
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.red)
+            
+                
+                Button("Add calories 10cal"){
+                    Task{await manager.addTodayEnergyBurned(calorie: 10)}
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.cyan)
+                
+                
+            }
+            
+        }
     }
 }
 
 #Preview {
     ContentView()
 }
+
