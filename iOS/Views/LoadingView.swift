d0a51a2e5cf2db1324952c09dcd1f6f29d7cd99b
Averina Harseno Subianto <156516558+lunarlife2@users.noreply.github.com>
2026-07-03 17:29:04 +0800

    Create LoadingView.swift

---
+++ iOS/Views/LoadingView.swift
@@ -0,0 +1,14 @@
+//
+//  LoadingView.swift
+//  CH4
+//
+//  Created by Averina on 03/07/26.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+	var body: some View {
+		Text("Loading....")
+	}
+}
